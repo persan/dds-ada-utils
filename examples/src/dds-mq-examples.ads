cc97743781e9090696faa147f2ea09e1af3a1102
persan <per.s.sandberg@bahnhof.se>
2021-10-05 07:41:53 +0200

    Remove unused withs.

--- examples/src/dds-mq-examples.ads
+++ examples/src/dds-mq-examples.ads
@@ -1,10 +1,3 @@
-with Dds.Builtin_String_TypeSupport;
-with Dds.Builtin_String_DataReader;
-with Dds.Builtin_String_DataWriter;
-
-with DDS.Mq.Messaging_Generic;
-with DDS.Mq.Messaging_Generic.Client_Generic;
-with DDS.Mq.Messaging_Generic.Server_Generic;
 
 package DDS.Mq.Examples is
 
