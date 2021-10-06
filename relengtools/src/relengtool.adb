c5605540a55861b737b9d3f864c0ed1a264ed5b5
persan <per.s.sandberg@bahnhof.se>
2021-10-06 07:23:43 +0200

    Update releng tool

--- relengtools/src/relengtool.adb
+++ relengtools/src/relengtool.adb
@@ -1,34 +1,74 @@
+with Ada.Command_Line;
+with Ada.Directories;
+with Ada.Strings.Fixed;
+with Ada.Strings.Unbounded;
 with Ada.Text_IO;
 with DDS.Mq;
-with Ada.Command_Line;
 with GNAT.Expect;
+with GNAT.Formatted_String;
 with GNAT.OS_Lib;
+with GNAT.Regpat;
+with GNAT.String_Split;
 with GNAT.Strings;
-with Ada.Strings.Unbounded;
 with GNATCOLL.Opt_Parse;
 
 procedure Relengtool is
    use Ada.Text_IO;
    use GNAT.OS_Lib;
-   OK : Boolean := True;
+   use Ada.Strings.Unbounded;
+
    package Arg is
       use GNATCOLL.Opt_Parse;
-      use Ada.Strings.Unbounded;
       Parser : Argument_Parser := Create_Argument_Parser
         (Help => "Relengtool");
 
       package Tag is new Parse_Flag
         (Parser => Parser,
          Long   => "--tag",
-         Help   => "git tag-");
+         Help   => "git tag.");
 
       package Version is new Parse_Flag
         (Parser => Parser,
          Long   => "--version",
          Help   => "Print version and quit");
 
+      package Verbose is new Parse_Flag
+        (Parser => Parser,
+         Short  => "-v",
+         Long   => "--verbose",
+         Help   => "Be verbose.");
+
+      package Patch is new Parse_Flag
+        (Parser => Parser,
+         Long   => "--patch",
+         Help   => "Bump patch version.");
+
+      package Minor is new Parse_Flag
+        (Parser => Parser,
+         Long   => "--minor",
+         Help   => "Bump Minor version.");
+
+      package Major is new Parse_Flag
+        (Parser => Parser,
+         Long   => "--major",
+         Help   => "Bump major version.");
+      Default_RelengFile : constant String := ".releng";
+      package RelengFile is new Parse_Option (Parser      => Parser,
+                                              Long        => "--relengfile",
+                                              Help        => "Files containing version numbers to be updated default; """ & Default_RelengFile & """.",
+                                              Arg_Type    => Unbounded_String,
+                                              Default_Val => To_Unbounded_String (Default_RelengFile));
+
    end Arg;
 
+   GIT    : constant GNAT.OS_Lib.String_Access := Locate_Exec_On_Path ("git");
+   OK     : Boolean := True;
+   procedure Log_Line (Line : String) is
+   begin
+      if Arg.Verbose.Get then
+         Put_Line (Line);
+      end if;
+   end;
    procedure Check_Version_Binary is
       pragma Warnings (Off);
    begin
@@ -37,11 +77,12 @@ procedure Relengtool is
          Put_Line (Standard_Error, "Declared project version '" & $VERSION & "' Mismatch with source version => '" & DDS.Mq.VERSION & "'.");
          Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
          OK := False;
+      else
+         Log_Line ("Declared project version '" & $VERSION & "' Matches with source version => '" & DDS.Mq.VERSION & "'.");
       end if;
    end Check_Version_Binary;
 
    procedure Check_Dirty is
-      GIT    : String_Access := Locate_Exec_On_Path ("git");
       Status : aliased Integer;
 
       use GNAT.Expect;
@@ -51,13 +92,101 @@ procedure Relengtool is
          Put_Line (Standard_Error, Get_Command_Output (GIT.all, Argument_String_To_List ("status").all, Input => "", Status => Status'Access));
          Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
          OK := False;
+      else
+         Log_Line ("Folder is clean.");
       end if;
    end;
-   procedure TAG is
+   use all type GNAT.Regpat.Regexp_Flags;
+   Matcher : GNAT.Regpat.Pattern_Matcher := GNAT.Regpat.Compile
+     (Expression =>  "(.*VERSION(\s*:\s*constant\s+Standard.String|)\s*:=\s*"")\d+\.\d+\.\d+(""\s*;.*)",
+      Flags      => GNAT.Regpat.Multiple_Lines + GNAT.Regpat.Case_Insensitive + GNAT.Regpat.Single_Line);
+   Matches : GNAT.Regpat.Match_Array (1 .. GNAT.Regpat.Paren_Count (Matcher));
+
+   function Next (S : String) return String is
+      use GNAT.String_Split;
+      use GNAT.Formatted_String;
+      Elements : GNAT.String_Split.Slice_Set := Create (S, ".");
+      Patch    : Integer := Integer'Value (Slice (Elements, 3));
+      Minor    : Integer := Integer'Value (Slice (Elements, 2));
+      Major    : Integer := Integer'Value (Slice (Elements, 1));
+      Format   : GNAT.Formatted_String.Formatted_String := +("%d.%d.%d");
    begin
-      if OK then
-         null;
+      if Arg.Patch.Get then
+         Patch := Patch + 1;
       end if;
+      if Arg.Minor.Get then
+         Minor := Minor + 1;
+         Patch := 0;
+
+      end if;
+      if Arg.Major.Get then
+         Major := Major + 1;
+         Minor := 0;
+         Patch := 0;
+      end if;
+      return -(Format & Major & Minor & Patch);
+   end;
+
+   procedure Update_Version is
+      procedure Update_File (Path : String) is
+         F           : GNAT.OS_Lib.File_Descriptor := GNAT.OS_Lib.Open_Read (Path, GNAT.OS_Lib.Text);
+         Buffer_Size : Natural := Natural (GNAT.OS_Lib.File_Length (F));
+         Buffer      : String (1 .. Buffer_Size);
+         Last        : Integer;
+         V           : constant String := Next ($VERSION);
+      begin
+         Log_Line ("Update Version number in : """ & Path & """ to """ & V & """.");
+         Last := GNAT.OS_Lib.Read (F, Buffer'Address, Buffer'Length);
+         GNAT.OS_Lib.Close (F);
+         GNAT.Regpat.Match (Matcher, Buffer, Matches);
+
+         F := GNAT.OS_Lib.Create_File (Path, GNAT.OS_Lib.Text);
+         Last := GNAT.OS_Lib.Write (F, Buffer (Matches (1).First)'Address, Matches (1).Last - Matches (1).First + 1);
+         Last := GNAT.OS_Lib.Write (F, V'Address, V'Length);
+         Last := GNAT.OS_Lib.Write (F,  Buffer (Matches (3).First)'Address, Matches (3).Last - Matches (3).First + 1);
+         GNAT.OS_Lib.Close (F);
+      end Update_File;
+   begin
+      if Ada.Directories.Exists (To_String (Arg.RelengFile.Get)) then
+         declare
+            Inf : Ada.Text_IO.File_Type;
+         begin
+            Log_Line ("Using releng file """ & To_String (Arg.RelengFile.Get) & """.");
+            Open (Inf, In_File, To_String (Arg.RelengFile.Get));
+            while not End_Of_File (Inf) loop
+               declare
+                  Line : constant String := Ada.Strings.Fixed.Trim (Get_Line (Inf), Ada.Strings.Both);
+               begin
+                  if Line'Length > 2 and then
+                    Line (Line'First .. Line'First + 1) /= "--" and then
+                    Line (Line'First) /= ';'  and then
+                    Line (Line'First) /= '#'
+                  then
+                     if Ada.Directories.Exists (Line) then
+                        Update_File (Line);
+                     else
+                        Put_Line (Standard_Error, "File :" & Line & " Not found");
+                        Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
+                        OK := False;
+                     end if;
+                  end if;
+               end;
+            end loop;
+            Close (Inf);
+         end;
+      else
+         Put_Line (Standard_Error, "No releng file """ & To_String (Arg.RelengFile.Get & """ found."));
+         Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
+         OK := False;
+      end if;
+   end Update_Version;
+
+   procedure TAG is
+      use GNAT.Expect;
+      Status : aliased Integer;
+   begin
+      Log_Line ("Adding tag: """ & $PROJECT & "-" & $VERSION & """.");
+      Put_Line (Standard_Error, Get_Command_Output (GIT.all, Argument_String_To_List ("tag -f " & $PROJECT & "-" & $VERSION).all, Input => "", Status => Status'Access));
    end TAG;
 begin
    if Arg.Parser.Parse then
@@ -67,8 +196,11 @@ begin
       end if;
       Check_Version_Binary;
       Check_Dirty;
-      if Arg.Tag.Get then
+      if OK and then Arg.Tag.Get then
          TAG;
+      end if;
+      if OK and then (Arg.Patch.Get or else Arg.Minor.Get or else Arg.Major.Get) then
+         Update_Version;
       end if;
    end if;
 end Relengtool;
